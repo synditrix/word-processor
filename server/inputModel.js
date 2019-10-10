f52243cc8f1175c32a84c594875cc2f08c4e2fa1
synditrix <no@levy-ve703-13406.apn.wlan.private.upenn.edu>
2019-10-09 22:31:23 -0400

    Adding server

---
+++ server/inputModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let Input = new Schema({
+    input_text: {
+        type: String
+    },
+    input_output: {
+        type: Object
+    },
+});
+
+module.exports = mongoose.model('Input', Input);
---
+++ server/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const inputRoutes = express.Router();
+const PORT = 4000;
+
+let processor = require('./processor.js');
+let Input = require('./inputModel');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/inputs', { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+
+inputRoutes.route('/').get(function(req, res) {
+    Input.find(function(err, inputs) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(inputs);
+        }
+    });
+});
+
+inputRoutes.route('/:id').get(function(req, res) {
+    let id = req.params.id;
+    Input.findById(id, function(err, input) {
+        res.json(input);
+    });
+});
+
+inputRoutes.route('/add').post(function(req, res) {
+	req.body.input_output = processor.processText(req.body.input_text);
+    let input = new Input(req.body);
+    input.save()
+        .then(todo => {
+            res.status(200).json({'input': 'input added successfully'});
+        })
+        .catch(err => {
+            res.status(400).send('adding new input failed');
+        });
+});
+
+app.use('/inputs', inputRoutes);
+
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+});
